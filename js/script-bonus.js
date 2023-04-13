5a9df9cb5b93a2cc0e5330deac1da1e3b39f32ff
valeriofilareti <valerio.filareti@gmail.com>
2023-04-13 17:32:06 +0200

    first commit

---
+++ js/script-bonus.js
@@ -0,0 +1,33 @@
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return {
+      immagini: [
+        'img/01.webp',
+        'img/02.webp',
+        'img/03.webp',
+        'img/04.webp',
+        'img/05.webp'
+      ],
+      counter: 0,
+    }
+  },
+  methods: {
+    next(){
+      if (this.counter < this.immagini.length -1) {
+        this.counter++;
+      } else {
+        this.counter = 0;
+      }
+    },
+    prev(){
+      if (this.counter === 0) {
+        this.counter = this.immagini.length -1;
+      } else {
+        this.counter--;
+      }
+    }
+  }
+
+}).mount('#app')
